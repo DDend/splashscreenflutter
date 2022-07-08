84613c45493977fe76bec7bddb6082f19aa5b186
DDend <45689523+DDend@users.noreply.github.com>
2022-07-08 11:05:01 +0700

    pertama

---
+++ splashscreen.dart
@@ -0,0 +1,46 @@
+import 'package:diskominfo/screen/home.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => MyHomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/img/splash.png",
+              width: 500,
+              height: 250,
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
